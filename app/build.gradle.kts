8d8a2b30a08033402cba69da3362472ba89e2c6e
BerkTolgaSuntay123 <btolgasuntay@yahoo.com>
2024-12-08 11:56:36 -0800

    2nd commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.homework3berksuntay"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.homework3berksuntay"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/homework3berksuntay/ResultsActivity.kt
+++ app/src/main/java/com/example/homework3berksuntay/ResultsActivity.kt
@@ -1,31 +1,20 @@
 package com.example.homework3berksuntay
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        // Handle system bars and padding
+        setContentView(R.layout.activity_results)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Add button navigation logic
-        val button = findViewById<Button>(R.id.btnClickMe)
-        button.setOnClickListener {
-            val intent = Intent(this, ResultsActivity::class.java)
-            startActivity(intent)
         }
     }
 }
